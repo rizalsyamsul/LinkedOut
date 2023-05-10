2e69cc4bd35ea7c762ae0f33cbddb9836952d27d
roby syahbani <robysyahbani01@gmail.com>
2023-05-10 21:50:04 +0700

    "Memperbaiki relasi model"

--- models/favourite.js
+++ models/favourite.js
@@ -11,8 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Favourite.belongsTo(models.Post)
-      Favourite.belongsTo(models.User)
     }
   }
   Favourite.init({
